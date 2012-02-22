025298bc685ddedaa9106eb2b7e9f0539db1e7aa
William <wwedler@riseup.net>
2012-02-21 22:30:01 -0500

    initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikeBinder::Application.config.secret_token = 'af6fc9be3ac2bf3e9825820bc199dec2581ff7d105690f1e623be59a7d057bde59b2fcfdf7cab27effc1f2f6af89e1a794576a8a3462fcbc40321feafebc91e8'
