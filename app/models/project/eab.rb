3a7e1b8fb3b98b99721649cc29f3e41666182434
William <wwedler@riseup.net>
2012-03-19 22:59:36 -0400

    updating docs

--- app/models/project/eab.rb
+++ app/models/project/eab.rb
@@ -1,3 +1,3 @@
-class Project::EAB < Project
-  has_one :spec, :as => :specable, :class_name => "Project::Spec::EAB"
+class Project::Eab < Project
+  has_one :spec, :as => :specable, :class_name => "Project::Spec::Eab"
 end
