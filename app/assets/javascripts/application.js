e57ec38648b98702e4c86ecc5b4031cd217bc570
Ryan Billings <rbilling@andrew.cmu.edu>
2012-09-17 00:16:52 -0400

    adding watermark for search box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,10 @@ $(document).ready(function(){
 	$("#newCommentWrap").toggle();
     });
 });
+
+function inputFocus(i){
+    if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
+}
+function inputBlur(i){
+    if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
+}
