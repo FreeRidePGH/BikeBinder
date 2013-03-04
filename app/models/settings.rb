10e61b0ea16661f2c1bf0b967062909faeb977ae
William Wedler <wwedler@riseup.net>
2013-03-04 10:35:34 -0500

    typo fix and checking for nil

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,4 @@
-pmodule Settings
+module Settings
   class LinearUnit
     
     def self.options
@@ -10,35 +10,34 @@ pmodule Settings
     end
 
     def self.to_persistance_units(q)
+      return nil if q.nil?
+
       units = self.persistance.units
       val = (q.respond_to?(:units)) ?
       q.convert_to(units) : Unit.new(q) * units
     end
-
+    
     def self.default_display
       Unit.new('inch')
     end
-
+    
     def self.default_input
       Unit.new('inch')
     end
-
+    
     def display
       self.class.default_display
     end
-
+    
     def input
       self.class.default_input
     end
-
+    
   end
-
+  
   class Color
     def self.options
       Select2BikeBinder.configuration.color_option_keys
     end
   end
 end # module Settings
-
-
-
