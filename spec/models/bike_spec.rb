0c363915d4ffc9457292b233b5f3942de5cdb3d4
William Wedler <wwedler@riseup.net>
2013-03-13 00:24:09 -0400

    adding test coverage

--- spec/models/bike_spec.rb
+++ spec/models/bike_spec.rb
@@ -114,48 +114,137 @@ describe Bike do
   context "without an assignment" do
     subject(:bike_not_asgnd){FactoryGirl.create(:bike)}
 
-    describe "that is in the shop" do
-      it "can not depart"
-      it "can be assigned"
+    context "that is in the shop" do
+      describe "departs" do
+        let(:departure){Departure.build(:bike => bike_not_asgnd, :value => 0)}
+
+        before :each do
+          departure.save
+        end
+        
+        it "has invalid departure" do
+          expect(departure).to_not be_valid
+        end
+
+        it "is not departed" do
+          expect(bike_not_asgnd).to_not be_departed
+        end
+      end
+      
+      describe "is assigned to a program" do
+        let(:program){FactoryGirl.create(:program)}
+        let(:assignment){Assignment.build(:bike => bike_not_asgnd, :program => program)}
+
+        before :each do
+          assignment.save
+        end
+        
+        it "references the program" do
+          expect(bike_not_asgnd.application).to eq program
+        end
+
+        it "is not available" do
+          expect(bike_not_asgnd).to_not be_available
+        end
+
+        it "is not departed" do
+          expect(bike_not_asgnd).to_not be_departed
+        end
+      end # describe "is assigned to a program"
     end
-  end
+  end #   context "without an assignment"
 
   context "the bike number is changed" do
-    before(:each) do
-      @bike = FactoryGirl.create(:bike)
-      @new_number = BikeNumber.format_number(10000+Bike.count+1)
-    end
+    subject(:bike){FactoryGirl.create(:bike)}
+    let(:new_number){FactoryGirl.generate(:bike_number)}
 
-    it "is allowed" do
-      @bike.number = @new_number
-      expect(@bike).to be_valid
+    before :each do
+      bike.number = new_number
     end
-  end
+    
+    it "is allowed" do
+      expect(bike).to be_valid
+    end
+  end # context "the bike number is changed"
 
 
   context "with an assignment" do
-    before (:each) do
-      @bike = FactoryGirl.create(:bike)
+    let(:assignment){FactoryGirl.create(:assignment)}
+    subject(:bike){assignment.bike}
+
+
+    it "is not available" do
+      expect(bike).to_not be_available
     end
 
-    it "is not available"
+    it "references the assignment" do
+      expect(bike.assignment).to eq assignment
+    end
 
-    it "can not be assigned"
+    describe "being assigned" do
+      let(:program){FactoryGirl.create(:program)}
+      let(:new_assignment){Assignment.build(:bike => bike, :program => program)}
+
+      before :each do
+        new_assignment.save
+        bike.reload
+      end
+      
+      it "is not valid" do
+        expect(new_assignment).to_not be_valid
+      end
+
+      it "does not change the bike assignment" do
+        expect(bike.assignment).to_not eq new_assignment
+        expect(bike.assignment).to eq assignment
+      end
+    end
 
     context "which is canceled" do
 
-      it "is be available" 
+      before :each do
+        bike.assignment.delete
+        bike.reload
+      end
 
-      it "has no assignment"
+      it "is be available" do
+        expect(bike).to be_available
+      end
+
+      it "has no assignment" do
+        expect(bike.assignment).to be_nil
+      end
 
       it "has a record of the canceled assignment"
       
-    end
+    end # context "which is canceled" do
     
     context "in the shop" do    
-
       it "can be deleted" do
-        expect {@bike.destroy.to change(Bike, :count).by(-1)}
+        expect {bike.destroy.to change(Bike, :count).by(-1)}
+      end
+    end # context "in the shop" do
+  end # context "with an assignment"
+
+
+  context "which is departed" do
+    let(:assignment){FactoryGirl.create(:assignment)}
+    subject(:bike){assignment.bike}
+    let(:departure){Departure.build(:bike => bike, :value => 0)}
+
+    before :each do
+      departure.save
+    end
+
+    describe "deleting assignment" do
+
+      before :each do
+        bike.assignment.destroy
+        bike.reload
+      end
+      
+      it "is returns the bike to the sop" do
+        expect(bike).to_not be_departed
       end
 
     end
