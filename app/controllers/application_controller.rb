8f49800f986b4dca24344bb0061d230261f5de11
William Wedler <wwedler@riseup.net>
2013-04-05 20:42:35 -0400

    ensure authorization for each action in the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #check_authorization
+  check_authorization :unless => :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
