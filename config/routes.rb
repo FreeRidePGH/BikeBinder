26c29f90c5929b0e15768e126bc2e02df98e5a41
William Wedler <wwedler@riseup.net>
2013-06-13 22:17:44 -0400

    cleaning routes file

--- config/routes.rb
+++ config/routes.rb
@@ -26,33 +26,6 @@ BikeBinder::Application.routes.draw do
   # Search routes
   resources :searches, :only => [:index]
 
-  if false
-  resources :programs, :except => [:destroy] do
-    resources :bikes, :only => [:index]
-  end
-
-  resources :destinations, :except => [:destroy] do
-    resources :bikes, :only => [:index]
-  end
-  end
-
-  # Mobile routes
-  if false
-    # See README for mobile strategy.
-    resources :mobile, :only => [:index]
-    match '/mobile/filter_bikes' => 'mobile#filter_bikes', :via => [:get]
-    match '/mobile/home' => 'mobile#home'
-    match '/mobile/find' => 'mobile#find'
-    match '/mobile/add' => 'mobile#add'
-    match '/mobile/show/:id' => 'mobile#show', :id => BikeNumber.pattern
-    match '/mobile/findsubmit' => 'mobile#find_submit'
-    match '/mobile/generate_code/:id' => 'mobile#generate_code'
-    match '/mobile/upload' => 'mobile#upload', :via => [:get,:post]
-    match '/mobile/ajax_show/:id' => 'mobile#ajax_show'
-    match '/mobile/ajax_find' => 'mobile#ajax_find', :via => :get
-    match '/mobile/ajax_add' => 'mobile#ajax_add', :via => :get
-  end
-
   ####################################################
   ## FINAL ROUTES TO CATCH UNMATCHED URIs AND SEARCHES
   ####################################################
