f74f7cc04a3436c2e12796b01632849e9ff2a224
William Wedler <wwedler@riseup.net>
2014-10-04 08:02:48 -0700

    rename index needs the name as a string or it fails silently

--- db/migrate/20141004143056_fix_departures_disposition_column_name.rb
+++ db/migrate/20141004143056_fix_departures_disposition_column_name.rb
@@ -3,6 +3,6 @@ class FixDeparturesDispositionColumnName < ActiveRecord::Migration
     rename_column :departures, :application_id, :disposition_id
     rename_column :departures, :application_type, :disposition_type
 
-    rename_index :departures, :index_departures_on_application, :index_departures_on_disposition
+    rename_index :departures, "index_departures_on_application", "index_departures_on_disposition"
   end
 end
