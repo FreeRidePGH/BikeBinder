0aeb911fd1692b74e0de63e72a506f14f7a6543a
William Wedler <wwedler@riseup.net>
2014-10-10 21:45:50 -0400

    load airbrake api from text file

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,16 @@
+#require File.join(File.dirname(__FILE__) + "../", "config/directories.rb")
+
+require File.expand_path(File.join("..","..","directories.rb"), __FILE__)
+
+puts File.expand_path(File.join("..","..","directories.rb"), __FILE__)
+
 Airbrake.configure do |config|
-  config.api_key = ENV['ERRAPIKEY']
+  if ENV["ERR_API_KEY"].present?
+    config.api_key = ENV["ERR_API_KEY"].strip
+  elsif File.exists?(ERR_API_KEY)
+    config.api_key = File.read(File.expand_path(ERR_API_KEY)).strip
+  end
+
   config.host    = 'err.frbp.org'
   config.port    = 80
   config.secure  = config.port == 443
