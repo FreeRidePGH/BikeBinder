a8d874a760ed09c075b66dbcb2b5b7a8ed3f2828
William Wedler <wwedler@riseup.net>
2015-02-21 11:43:33 -0500

    fix rspec configuration that was breaking guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,18 +111,6 @@ end
 
 # See http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
 Devise.stretches = 1
-class ActiveRecord::Base
-  mattr_accessor :shared_connection
-  @@shared_connection = nil
-
-  def self.connection
-    @@shared_connection || retrieve_connection
-  end
-end
-
-# Forces all threads to share the same connection. This works on
-# Capybara because it starts the web server in a thread.
-ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
 # --- Instructions ---
 # Sort the contents of this file into a Spork.prefork and a Spork.each_run
